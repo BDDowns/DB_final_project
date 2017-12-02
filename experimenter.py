e8713e06dcb15a2cd9c165d24fa65b15129ad5b0
Bryan Downs <xerobasix@gmail.com>
2017-12-02 14:35:29 -0700

    initial commit

---
+++ experimenter.py
@@ -0,0 +1,69 @@
+'''
+Experimenter.py controls all experiments for the datascience portion of Databases.
+Each experiment is layed out such that it contains the actual code to perform analysis on the data
+and a seperate function to output a visual representation of said exeriment.
+
+@authors: Bryan Downs, Brett Shelley, Brendan Tracey
+
+'''
+
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+
+
+
+'''
+Experiment 1 analyzes the spearman rank correlation of length of movie and rating
+
+'''
+def experiment1():
+    pass
+
+
+def visual1():
+    pass
+
+
+'''
+Experiment 2 analyzes the correlation of start year and budget for a movie, using the pearson correlation coefficient
+
+'''
+def experiment2():
+    pass
+
+
+def visual2():
+    pass
+
+'''
+Experiment 3 performs a regression analysis of the rating and runtime of a television series in an attempt to forcast
+the performance of the next season
+
+'''
+def experiment3():
+    pass
+
+
+def visual3():
+    pass
+
+'''
+Experiment 4 performs a cluster analysis of movies based on _______ to determine similar films
+
+'''
+def experiment4():
+    pass
+
+def visual4():
+    pass
+
+'''
+Experiment 5 looks at association of actors and directors by performing a market basket analysis
+
+'''
+def experiment5():
+    pass
+
+def visual5():
+    pass
